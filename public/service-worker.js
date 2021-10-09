0e70fb1e2e12c1ed941bea0e87265f4cea68082b
Arne Tripolone <arnetripolone@gmail.com>
2021-10-09 15:39:36 +1100

    updated service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,13 +13,14 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 //Install
 self.addEventListener("install", function (evt) {
-evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-    console.log("Your files were pre-cached successfully.")
-    .then(self.skipWaiting())
-    })
-)
-})
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+    self.skipWaiting();
+  });
 
 //Activate
 self.addEventListener("activate", function (evt) {
