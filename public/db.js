4ed4a9d2ac3bd000bfda2acb5b4c8120d9ec0eaa
Arne Tripolone <arnetripolone@gmail.com>
2021-10-09 17:22:46 +1100

    updated db.js

--- public/db.js
+++ public/db.js
@@ -14,7 +14,7 @@ request.onsuccess = function(event) {
 };
 
 request.onerror = function(event) {
-  console.log("Woops! " + event.target.errorCode);
+  console.log("error" + event.target.errorCode);
 };
 
 function saveRecord(record) {
@@ -23,3 +23,28 @@ function saveRecord(record) {
   store.add(record);
 }
 
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      });
+    }
+  };
+}
+window.addEventListener("online", checkDatabase);
