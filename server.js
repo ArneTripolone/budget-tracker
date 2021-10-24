3f11b51fc7df16304e48bea20656015971a3f680
Arne Tripolone <arnetripolone@gmail.com>
2021-10-24 14:29:46 +1100

    make it better

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const uri = process.env.MONGODB_URI || "mongodb://localhost/budget";
+MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
